4c58e002c26a728915b5f139ad67358aa82f9917
guillaume806 <guillaumebodart@outlook.fr>
2023-08-22 15:25:01 +0200

    firstcommit

---
+++ src/main/java/org/example/model/Object.java
@@ -0,0 +1,13 @@
+package org.example.model;
+
+import java.util.Date;
+
+public class Object {
+
+    private String description;
+
+    private Date finalDate;
+
+    private enum priority;
+
+}
---
+++ src/main/java/org/example/model/Priority.java
@@ -0,0 +1,5 @@
+package org.example.model;
+
+public enum Priority {
+    hight, medium, low
+}
